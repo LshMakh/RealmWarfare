08278c8cb8355200f0afab3bd3e4d05cd9d68307
LshMakh <lasha.makharashvili01@gmail.com>
2026-03-22 04:35:05 +0400

    feat: add generic object pool system

---
+++ scripts/systems/object_pool.gd
@@ -0,0 +1,52 @@
+class_name ObjectPool
+extends Node
+
+@export var pooled_scene: PackedScene
+@export var pool_size: int = 50
+
+var _available: Array[Node] = []
+var _all: Array[Node] = []
+
+
+func _ready() -> void:
+	for i in range(pool_size):
+		_create_instance()
+
+
+func _create_instance() -> Node:
+	var instance: Node = pooled_scene.instantiate()
+	add_child(instance)
+	instance.visible = false
+	instance.set_process(false)
+	instance.set_physics_process(false)
+	if instance.has_method("set_pool"):
+		instance.set_pool(self)
+	_available.append(instance)
+	_all.append(instance)
+	return instance
+
+
+func get_instance() -> Node:
+	var instance: Node
+	if _available.is_empty():
+		instance = _create_instance()
+		_available.erase(instance)
+	else:
+		instance = _available.pop_back()
+	instance.visible = true
+	instance.set_process(true)
+	instance.set_physics_process(true)
+	return instance
+
+
+func release(instance: Node) -> void:
+	instance.visible = false
+	instance.set_process(false)
+	instance.set_physics_process(false)
+	if instance.has_method("reset"):
+		instance.reset()
+	_available.append(instance)
+
+
+func active_count() -> int:
+	return _all.size() - _available.size()
